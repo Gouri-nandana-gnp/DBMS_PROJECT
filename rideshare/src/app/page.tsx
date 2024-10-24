07671c8e36a61242063cabf4f5f207f3f6992a0c
Gouri-nandana-gnp <gourinandanapp@gmail.com>
2024-10-24 23:44:06 +0530

    frontend 50% done,backend started

--- rideshare/src/app/page.tsx
+++ rideshare/src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import UberLanding from './(admin)/login/page';
 import HomePage from './(admin)/home/page';
+import TableComponent from './(admin)/notification/page';
 
 const page = () => {
   return (
     <div>
       <HomePage />
+      <TableComponent/>
     </div>
   );
 }
